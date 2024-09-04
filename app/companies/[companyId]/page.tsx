72b13040932d270cf2e7e32499a38facb2fde312
Faizan Rao <rao.faizanaries1920@gmail.com>
2024-09-04 20:45:41 +0500

    page/company-events

---
+++ app/companies/[companyId]/page.tsx
@@ -0,0 +1,136 @@
+"use client";
+import CompanyEventCard from "@/components/CompanyEventCard";
+import {
+  AtSign,
+  Facebook,
+  Instagram,
+  Linkedin,
+  MapPin,
+  Phone,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const CompaniesEvent = () => {
+  return (
+    <div className="flex flex-col flex-wrap">
+      <div className="flex flex-col  bg-[#7655fa] py-10 min-h-[450px]">
+        {/* Header Container*/}
+        <div className="container">
+          <div className="flex-1 flex justify-between items-center gap-4 pb-4 border-b-[1px] border-[#FFFFFFB2]  text-white">
+            <h1 className="font-semibold text-2xl">EvenTeam</h1>
+            <h1 className="font-semibold text-lg">Event Management System</h1>
+          </div>
+
+          <div className="flex-1 min-h-[250px] flex justify-between items-center gap-10 pb-4   text-white">
+            {/* Company Name & Logo */}
+            <div className="flex gap-4 items-center">
+              <span className="flex justify-center items-center ">
+                <Image
+                  src={"/profile_logo.svg"}
+                  height={75}
+                  width={75}
+                  alt="company_logo"
+                />
+              </span>
+
+              <div className="flex flex-col my-10">
+                <h1 className="text-white font-semibold text-3xl">
+                  Company Name
+                </h1>
+                <h1 className="text-[#FFFFFFB2] font-semibold text-lg">
+                  Company events
+                </h1>
+              </div>
+            </div>
+            {/* Company Details */}
+            <div className="flex-1 flex flex-col gap-4 justify-center-center">
+              <div className="flex  gap-4">
+                <span className="flex gap-4">
+                  <AtSign className="text-white" />
+                  <span className="text-white font-white ">
+                    company@gmail.com
+                  </span>
+                </span>
+                <span className="flex gap-4">
+                  <Phone className="text-white" />
+                  <span className="text-white font-white ">
+                    company@gmail.com
+                  </span>
+                </span>
+              </div>
+              <span className="flex gap-4">
+                <MapPin className="text-white" />
+                <span className="text-white font-white ">
+                  789 Willowbrook Lane, Apt 22B, New Jersey Street, New York{" "}
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Main Event Publish Container */}
+      <div className="container flex flex-wrap  gap-10">
+        <CompanyEventCard />
+        <CompanyEventCard />
+        <CompanyEventCard />
+        <CompanyEventCard />
+        <CompanyEventCard />
+        <CompanyEventCard />
+      </div>
+
+      <div className="flex flex-col  bg-[#7655fa] py-10 min-h-[250px]">
+        {/* Header Container*/}
+        <div className="container">
+          <div className="flex-1 flex justify-between items-center gap-4 pb-4 border-b-[1px] border-[#FFFFFFB2]  text-white">
+            <h1 className="font-semibold text-2xl">EvenTeam</h1>
+            <div className="flex gap-4">
+              <Image
+                src={"/social-icons/facebook.svg"}
+                alt="social-logo"
+                width={20}
+                height={25}
+              />
+              <Image
+                src={"/social-icons/insta.svg"}
+                alt="social-logo"
+                width={20}
+                height={25}
+              />
+              <Image
+                src={"/social-icons/x.svg"}
+                alt="social-logo"
+                width={20}
+                height={25}
+              />
+              <Image
+                src={"/social-icons/linkedIn.svg"}
+                alt="social-logo"
+                width={20}
+                height={25}
+              />
+              <Image
+                src={"/social-icons/youtube.svg"}
+                alt="social-logo"
+                width={20}
+                height={25}
+              />
+            </div>
+          </div>
+
+          <div className="flex-1 flex justify-between items-center gap-4 pb-4 my-8 text-white">
+            <h1 className=" text-lg">Copyright © 2024. All rights reserved</h1>
+            <div className="flex gap-4">
+            <Link href={"#"} className="text-white">Privacy Policy</Link>
+            <Link href={"#"} className="text-white">Terms & Conditions</Link>
+            <Link href={"#"} className="text-white">Return Policy</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompaniesEvent;
---
+++ components/CompanyEventCard.tsx
@@ -0,0 +1,58 @@
+import { ChevronRight, Clock, Droplet, MapPin, Watch } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const CompanyEventCard = () => {
+  return (
+    <div className="bg-[white]  hover:translate-y-[-30%] transition-all duration-300 flex-1 flex flex-col p-6  rounded-3xl translate-y-[-25%] min-w-[400px]  ">
+      <span className="bg-[#7655fa26]  text-base mb-4 mt-2 px-4 py-2 self-start text-center  rounded-full text-[#7655fa] font-semibold">
+        Upcoming
+      </span>
+      <h1 className="font-semibold text-xl mb-2">
+        New {"Year's"} Eve Celebration
+      </h1>
+      <p className=" text-[#999999] pb-4 border-b-[1px] ">
+        Ring in the new year with a grand celebration featuring live music,
+        dancing, and a spectacular fireworks display at midnight.{" "}
+      </p>
+      <div className="flex my-2 justify-between items-center">
+        <h1 className="text-[#999999] font-semibold">Important Times</h1>
+        <button className="bg-[#7655fa26] text-sm  px-3  mt-2  text-center p-2 rounded-full text-[#7655fa] font-semibold">
+          View Details
+        </button>
+      </div>
+
+      <div className="flex flex-1 pb-4 border-b-[1px] max-h-[140px] justify-stretch">
+        <span className=" flex-1 p-4 flex-col flex justify-center items-center rounded-md gap-4 bg-[#7655fa26]">
+          <Watch className="text-[#7655fa]" size={35} />
+
+          <span className="text-[#999999] text-lg font-semibold">
+            Start at 00:00
+          </span>
+        </span>
+      </div>
+      <div className="flex my-5 font-semibold flex-col gap-4">
+        <div className="flex gap-4">
+          <Clock className="text-[#7655fa] min-w-[30px]" />
+          <span className="text-sm text-[#999999]">8 AM, June 27th - 12 PM, June 29th</span>
+        </div>
+        <div className="flex gap-4">
+          <MapPin className="text-[#7655fa] min-w-[30px]" />
+          <span className="text-sm text-[#999999]">789 , New Jersey Street, New York </span>
+        </div>
+      </div>
+
+      <Link
+        href={"/events/1"}
+        className="flex text-base justify-center items-center bg-[#7655fa] rounded-full py-3 px-4"
+      >
+        <button className="  font-semibold text-center ">
+          <span className="text-white ">Register For Event</span>
+        </button>
+        <ChevronRight className="text-white" />
+      </Link>
+    </div>
+  );
+};
+
+export default CompanyEventCard;
