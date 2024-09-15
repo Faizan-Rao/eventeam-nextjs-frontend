9070856d993f14be6c022fcaf81c8d37c1fbed0f
Faizan Rao <rao.faizanaries1920@gmail.com>
2024-09-15 16:05:54 +0500

    feature/responsive-login-page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,13 +14,13 @@ const Login = () => {
     <>
       <div
         data-state={isLeft}
-        className="bg-[#7655fa]   aspect-square transition-all  duration-700 fixed w-full  rounded-full translate-y-[-50%]  [&[data-state=right]]:translate-x-[50%]  [&[data-state=left]]:translate-x-[-60%]"
+        className="bg-[#7655fa] sm:hidden md:block sm:h-[200vh] md:min-h-screen  aspect-square transition-all  duration-700 fixed w-full  rounded-full translate-y-[-50%]  [&[data-state=right]]:translate-x-[50%]  [&[data-state=left]]:translate-x-[-60%]"
       >
         Hello
       </div>
 
       {isLeft === "left" && (
-        <div className="flex min-h-screen bg-[white] justify-center border-[2px] ">
+        <div className="flex  min-h-screen bg-[white] justify-center border-[2px] ">
           <motion.div
             animate={{ x: ["-100%", "0%"], opacity: [0, 1] }}
             transition={{
@@ -28,9 +28,9 @@ const Login = () => {
               duration: 0.3,
               x: { duration: 0.4 },
             }}
-            className="flex-1 flex justify-center items-center z-20"
+            className="flex-1 sm:hidden md:flex justify-center mx-auto items-center z-20"
           >
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col sm:items-center gap-3">
               <h1 className="text-4xl text-white font-semibold">New Here?</h1>
               <h1 className="text-2xl text-white ">Join Us Right Now</h1>
               <button
@@ -52,13 +52,21 @@ const Login = () => {
             }}
             className="flex-1 "
           >
+             
+            <button
+                className=" sm:block md:hidden min-w-full my-4 border-[#7655fa]  mx-auto border-[2px] px-4 py-2 rounded-full text-[#7655fa] font-semibold"
+                onClick={() => setLeft("right")}
+              >
+                New here Sign up now
+              </button>
+            
             <LoginForm />
           </motion.span>
         </div>
       )}
 
       {isLeft === "right" && (
-        <div className="flex min-h-screen bg-[white] justify-center border-[2px] ">
+        <div className="flex  min-h-screen bg-[white] justify-center border-[2px] ">
           <motion.span
             animate={{ x: ["-100%", "0%"], opacity: [0, 1] }}
             transition={{
@@ -69,6 +77,12 @@ const Login = () => {
             }}
             className="flex-1 "
           >
+           <button
+                className=" sm:block md:hidden min-w-full my-4 border-[#7655fa]  mx-auto border-[2px] px-4 py-2 rounded-full text-[#7655fa] font-semibold"
+                onClick={() => setLeft("left")}
+              >
+                Already Member? Sign in now
+              </button>
             <SignupForm />
           </motion.span>
 
@@ -80,7 +94,7 @@ const Login = () => {
               x: { duration: 0.4 },
               opacity: { duration: 0.3 },
             }}
-            className="flex-1 flex justify-center items-center z-20"
+            className="flex-1 sm:hidden md:flex justify-center items-center z-20"
           >
             <div className="flex flex-col gap-3">
               <h1 className="text-4xl text-white font-semibold">
